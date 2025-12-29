e5cab1dd3294546a7cf020f0719912ae995ce656
User <user@example.com>
2025-12-30 01:19:53 +0400

    fix: hardcode base url, add loading screen, disable strict mode

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    base: mode === 'github' ? '/govender-app/' : '/',
+    base: '/govender-app/',
 }))
